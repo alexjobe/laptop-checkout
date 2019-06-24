6a2a8cea33acef1948ca1dca6f1907ef2de4b120
Alex <ajobe15@gmail.com>
2019-06-24 15:54:57 -0500

    Only highlight past due checkouts that have not been returned yet

--- public/app.js
+++ public/app.js
@@ -4,11 +4,8 @@ Array.from(checkouts).forEach(function(checkout){
     checkout.addEventListener("click", function(){
         editCheckout(checkout.dataset.checkid)
     });
-    if(new Date(checkout.dataset.etadate) < new Date()) {
+    if(new Date(checkout.dataset.etadate) < new Date() && checkout.dataset.returned === "false") {
         checkout.classList.add("overdue");
-    }
-    if(Boolean(checkout.dataset.returned)){
-        console.log("yes");
     }
 });
 
